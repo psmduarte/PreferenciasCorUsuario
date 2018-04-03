d7dbaafea877234cdb9661480c8f0c8c92fd9c8e
psmduarte <psmduarte@live.com>
2018-04-03 23:37:07 +0100

    Done

--- app/src/main/java/preferenciascorusuario/cursoandroid/com/preferenciascorusuario/MainActivity.java
+++ app/src/main/java/preferenciascorusuario/cursoandroid/com/preferenciascorusuario/MainActivity.java
@@ -1,17 +1,22 @@
 package preferenciascorusuario.cursoandroid.com.preferenciascorusuario;
 
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RelativeLayout layout;
     private RadioGroup radioGroup;
-    private RadioButton radioButtonSelecionado;
     private Button botaoSalvar;
+    private RadioButton radioButtonSelecionado;
+    private static final String ARQUIVO_PREFERENCIA = "ArqPref";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +25,39 @@ public class MainActivity extends AppCompatActivity {
 
         radioGroup = findViewById(R.id.radioGroupId);
         botaoSalvar = findViewById(R.id.botaoSalvarId);
-
+        layout = findViewById(R.id.layoutId);
         botaoSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
+                int idRadioButtonEscolhido = radioGroup.getCheckedRadioButtonId();
+
+                if(idRadioButtonEscolhido>0){
+                radioButtonSelecionado = findViewById(idRadioButtonEscolhido);
+
+                    SharedPreferences sharedPreferences = getSharedPreferences(ARQUIVO_PREFERENCIA,0);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    String corEscolhida=radioButtonSelecionado.getText().toString();
+                    editor.putString("corEscolhida", corEscolhida);
+                    editor.apply();
+                    setBackground(corEscolhida);
+
+                }
             }
         });
+    }
+
+    private void setBackground(String cor){
+        if(cor.equals("Azul")){
+            layout.setBackgroundColor(Color.parseColor("#495b7c"));
+
+        }else if(cor.equals("Laranja")){
+            layout.setBackgroundColor(Color.parseColor("#ffce26"));
+
+        }else if(cor.equals("Verde")){
+            layout.setBackgroundColor(Color.parseColor("#009670"));
+
+        }
+
     }
 }
